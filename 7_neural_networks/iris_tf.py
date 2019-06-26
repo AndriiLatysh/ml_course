5bd8070b6f3e2aa3ed1a1cb98370d744a059fc72
alatys009 <andrii_latysh@comcast.com>
2019-06-26 18:52:37 +0300

    TF example refactored.

--- 7_neural_networks/iris_tf.py
+++ 7_neural_networks/iris_tf.py
@@ -8,28 +8,28 @@ import matplotlib.pyplot as plt
 
 
 # Create and train a tensorflow model of a neural network
-def create_train_model(hidden_nodes, num_iters):
+def create_train_model(input_nodes, hidden_nodes, output_nodes, num_iters):
     # Reset the graph
     tf.reset_default_graph()
 
     # Placeholders for input and output data
-    X = tf.placeholder(shape=(len(X_train), 4), dtype=tf.float64, name="X")
-    y = tf.placeholder(shape=(len(y_train), 3), dtype=tf.float64, name="y")
+    X = tf.placeholder(shape=(len(X_train), input_nodes), dtype=tf.float64, name="X")
+    y = tf.placeholder(shape=(len(y_train), output_nodes), dtype=tf.float64, name="y")
 
     # Variables for two group of weights between the three layers of the network
-    W1 = tf.Variable(np.random.rand(4, hidden_nodes), dtype=tf.float64)
-    W2 = tf.Variable(np.random.rand(hidden_nodes, 3), dtype=tf.float64)
+    W1 = tf.Variable(np.random.rand(input_nodes, hidden_nodes), dtype=tf.float64)
+    W2 = tf.Variable(np.random.rand(hidden_nodes, output_nodes), dtype=tf.float64)
 
     # Create the neural net graph
-    A1 = tf.sigmoid(tf.matmul(X, W1))
-    y_est = tf.sigmoid(tf.matmul(A1, W2))
+    H1 = tf.sigmoid(tf.matmul(X, W1))
+    y_est = tf.sigmoid(tf.matmul(H1, W2))
 
     # Define a loss function
     deltas = tf.square(y_est - y)
     loss = tf.reduce_sum(deltas)
 
     # Define a train operation to minimize the loss
-    optimizer = tf.train.GradientDescentOptimizer(0.005) #0.005
+    optimizer = tf.train.GradientDescentOptimizer(0.005)  # 0.005
     train = optimizer.minimize(loss)
 
     # Initialize variables and run session
@@ -37,14 +37,15 @@ def create_train_model(hidden_nodes, num_iters):
     sess = tf.Session()
     sess.run(init)
 
-    # Go through num_iters iterations
+    # Go through number_of_iters iterations
     for i in range(num_iters):
         sess.run(train, feed_dict={X: X_train, y: y_train})
         loss_plot[hidden_nodes].append(sess.run(loss, feed_dict={X: X_train, y: y_train}))
         weights1 = sess.run(W1)
         weights2 = sess.run(W2)
 
-    print("loss (hidden nodes: {0}, iterations: {1}): {2:0.2f}".format(hidden_nodes, num_iters, loss_plot[hidden_nodes][-1]))
+    print("loss (hidden nodes: {0}, iterations: {1}): {2:0.2f}".format(hidden_nodes, num_iters,
+                                                                       loss_plot[hidden_nodes][-1]))
     sess.close()
     return weights1, weights2
 
@@ -78,31 +79,32 @@ X = standard_scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True)
 
-
 # Run the training for 3 different network architectures: (4-5-3) (4-10-3) (4-20-3)
 
 # Plot the loss function over iterations
-num_iters = 5000
-num_hidden_nodes = [1, 10, 30] # [5, 10, 20]
-loss_plot = {num_hidden_nodes[0]: [], num_hidden_nodes[1]: [], num_hidden_nodes[2]: []}
-weights1 = {num_hidden_nodes[0]: None, num_hidden_nodes[1]: None, num_hidden_nodes[2]: None}
-weights2 = {num_hidden_nodes[0]: None, num_hidden_nodes[1]: None, num_hidden_nodes[2]: None}
+number_of_iters = 5000
+input_nodes = 4
+output_nodes = 3
+number_of_hidden_nodes = [1, 10, 30]  # [5, 10, 20]
+loss_plot = {number_of_hidden_nodes[0]: [], number_of_hidden_nodes[1]: [], number_of_hidden_nodes[2]: []}
+weights1 = {number_of_hidden_nodes[0]: None, number_of_hidden_nodes[1]: None, number_of_hidden_nodes[2]: None}
+weights2 = {number_of_hidden_nodes[0]: None, number_of_hidden_nodes[1]: None, number_of_hidden_nodes[2]: None}
 
 plt.figure(figsize=(12, 8))
-for hidden_nodes in num_hidden_nodes:
-    weights1[hidden_nodes], weights2[hidden_nodes] = create_train_model(hidden_nodes, num_iters)
-    plt.plot(range(num_iters), loss_plot[hidden_nodes], label="nn: 4-{}-3".format(hidden_nodes))
+for hidden_nodes in number_of_hidden_nodes:
+    weights1[hidden_nodes], weights2[hidden_nodes] = create_train_model(input_nodes, hidden_nodes, output_nodes,
+                                                                        number_of_iters)
+    plt.plot(range(number_of_iters), loss_plot[hidden_nodes], label="nn: 4-{}-3".format(hidden_nodes))
 
 plt.xlabel("Iteration", fontsize=12)
 plt.ylabel("Loss", fontsize=12)
 plt.legend(fontsize=12)
 
 # Evaluate models on the test set
-X = tf.placeholder(shape=(len(X_test), 4), dtype=tf.float64, name="X")
-y = tf.placeholder(shape=(len(y_test), 3), dtype=tf.float64, name="y")
+X = tf.placeholder(shape=(len(X_test), input_nodes), dtype=tf.float64, name="X")
+y = tf.placeholder(shape=(len(y_test), output_nodes), dtype=tf.float64, name="y")
 
-for hidden_nodes in num_hidden_nodes:
-
+for hidden_nodes in number_of_hidden_nodes:
     # Forward propagation
     W1 = tf.Variable(weights1[hidden_nodes])
     W2 = tf.Variable(weights2[hidden_nodes])
