e3d7325b8008d2d8aa12fe6da1785afe0a199265
AndriiLatysh <andrii.latysh@gmail.com>
2019-10-21 23:36:07 +0300

    Regression demo updated with the second part.

---
+++ 2_regression/manual_calculation.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+subscribers_from_ads = pd.read_csv("subscribers_from_ads.csv")
+x = subscribers_from_ads["promotion_budget"].to_list()
+y = subscribers_from_ads["subscribers"].to_list()
+k = (len(x) * sum([a * b for a, b in zip(x, y)]) - sum(x) * sum(y)) / (len(x) * sum([a ** 2 for a in x]) - sum(x) ** 2)
+print(k)
+b = (sum(y) * sum([a ** 2 for a in x]) - sum(x) * sum([a * b for a, b in zip(x, y)])) / (
+            len(x) * sum([a ** 2 for a in x]) - sum(x) ** 2)
+b1 = (1 / len(x)) * sum(y) - (k / len(x)) * sum(x)
+print(b, b1)
--- 2_regression/subs_from_ads.py
+++ 2_regression/subs_from_ads.py
@@ -8,16 +8,15 @@ subscribers_from_ads = pd.read_csv("subscribers_from_ads.csv")
 print(subscribers_from_ads)
 plt.scatter(subscribers_from_ads["promotion_budget"], subscribers_from_ads["subscribers"])
 
-linear_model = lm.LinearRegression()
+linear_regression = lm.LinearRegression()
 promotion_budget = np.array(subscribers_from_ads["promotion_budget"]).reshape(-1, 1)
 number_of_subscribers = np.array(subscribers_from_ads["subscribers"]).reshape(-1, 1)
+linear_regression.fit(X=promotion_budget, y=number_of_subscribers)
 
-linear_model.fit(promotion_budget, number_of_subscribers)
+print(linear_regression.coef_)
+print(linear_regression.intercept_)
 
-print(linear_model.coef_)
-print(linear_model.intercept_)
-
-model = linear_model.predict(promotion_budget)
+model = linear_regression.predict(X=promotion_budget)
 
 plt.plot(promotion_budget, model)
 
