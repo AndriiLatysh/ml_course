b03299d23901dd166cea295d82527f74daaa6031
AndriiLatysh <andrii.latysh@gmail.com>
2020-04-30 23:30:28 +0300

    Clustering demo updated.

--- 7_clustering/anchor_clustering.py
+++ 7_clustering/anchor_clustering.py
@@ -11,7 +11,7 @@ fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
 object_sizes = pd.read_csv("data/object_sizes.csv")
 # plt.scatter(x=object_sizes["width"], y=object_sizes["height"])
 
-X = np.array(object_sizes[["width", "height"]]).reshape(-1, 2)
+X = object_sizes[["width", "height"]]
 
 # print(X)
 
@@ -24,19 +24,21 @@ kmeans_clustering_model.fit(X)
 kmeans_classes = kmeans_clustering_model.predict(X)
 
 ax1.set_title("K-means")
-ax1.scatter(x=X[:, 0], y=X[:, 1], c=kmeans_classes, cmap="prism")
+ax1.scatter(x=object_sizes["width"], y=object_sizes["height"], c=kmeans_classes, cmap="prism")
 
-kmeans_centroids = [(int(round(x)), int(round(y))) for x, y in kmeans_clustering_model.cluster_centers_]
-print(kmeans_centroids)
+kmeans_centroids = kmeans_clustering_model.cluster_centers_
+# print(kmeans_centroids)
+
+ax1.scatter(x=kmeans_centroids[:, 0], y=kmeans_centroids[:, 1], marker="X", color="k", s=100)
 
 kmeans_db_score = sk_metrics.davies_bouldin_score(X, kmeans_classes)
-print("Davies-Bouldin score: {0} (less is better).".format(kmeans_db_score))
+print("Davies-Bouldin score: {:.5g} (less is better).".format(kmeans_db_score))
 
 # kmeans_d_score = jqmcvi.dunn_fast(X, kmeans_classes)
 # print("Dunn score: {0} (more is better).".format(kmeans_d_score))
 
 kmeans_s_score = sk_metrics.silhouette_score(X, kmeans_classes)
-print("Silhouette score: {} (more is better).".format(kmeans_s_score))
+print("Silhouette score: {:.5g} (more is better).".format(kmeans_s_score))
 
 print("K-means++:")
 
@@ -47,15 +49,20 @@ kmeans_pp_clustering_model.fit(X)
 kmeans_pp_classes = kmeans_pp_clustering_model.predict(X)
 
 ax2.set_title("K-means++")
-ax2.scatter(x=X[:, 0], y=X[:, 1], c=kmeans_pp_classes, cmap="prism")
+ax2.scatter(x=object_sizes["width"], y=object_sizes["height"], c=kmeans_pp_classes, cmap="prism")
+
+kmeans_pp_centroids = kmeans_pp_clustering_model.cluster_centers_
+# print(kmeans_pp_centroids)
+
+ax2.scatter(x=kmeans_pp_centroids[:, 0], y=kmeans_pp_centroids[:, 1], marker="X", color="k", s=100)
 
 kmeans_pp_db_score = sk_metrics.davies_bouldin_score(X, kmeans_pp_classes)
-print("Davies-Bouldin score: {0} (less is better).".format(kmeans_pp_db_score))
+print("Davies-Bouldin score: {:.5g} (less is better).".format(kmeans_pp_db_score))
 
 # kmeans_pp_d_score = jqmcvi.dunn_fast(X, kmeans_pp_classes)
 # print("Dunn score: {0} (more is better).".format(kmeans_pp_d_score))
 
 kmeans_pp_s_score = sk_metrics.silhouette_score(X, kmeans_pp_classes)
-print("Silhouette score: {} (more is better).".format(kmeans_pp_s_score))
+print("Silhouette score: {:.5g} (more is better).".format(kmeans_pp_s_score))
 
 plt.show()
