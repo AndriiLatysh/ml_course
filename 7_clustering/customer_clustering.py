a13ad731ddab36b4f639816818a83dac470d4c1e
AndriiLatysh <andrii.latysh@gmail.com>
2020-05-02 22:41:31 +0300

    Minor corrections to clustering demo.

--- 7_clustering/customer_clustering.py
+++ 7_clustering/customer_clustering.py
@@ -13,13 +13,13 @@ def set_printing_options():
 
 set_printing_options()
 
-users = pd.read_csv("data/customer_online_closing_store.csv")
-users["return_rate"] = users["items_returned"] / users["items_purchased"]
-users["average_price"] = users["total_spent"] / users["items_purchased"]
+clients = pd.read_csv("data/customer_online_closing_store.csv")
+clients["return_rate"] = clients["items_returned"] / clients["items_purchased"]
+clients["average_price"] = clients["total_spent"] / clients["items_purchased"]
 
-print(users[["average_price", "return_rate", "overall_rating"]])
+print(clients[["average_price", "return_rate", "overall_rating"]])
 
-X = np.array(users[["average_price", "return_rate", "overall_rating"]]).reshape(-1, 3)
+X = np.array(clients[["average_price", "return_rate", "overall_rating"]]).reshape(-1, 3)
 min_max_scaler = sk_preprocessing.MinMaxScaler()
 X = min_max_scaler.fit_transform(X)
 
@@ -28,22 +28,21 @@ print(X)
 plt.title("Customer dendogram")
 
 linkage_method = "ward"
-dendogram = sp_clustering_hr.dendrogram(sp_clustering_hr.linkage(X, method=linkage_method))  # single complete average ward
+dendogram = sp_clustering_hr.dendrogram(
+    sp_clustering_hr.linkage(X, method=linkage_method))  # single complete average ward
 
 agglomerative_model = sk_clustering.AgglomerativeClustering(n_clusters=4, linkage=linkage_method)
 
 agglomerative_model.fit(X)
 
-classes = agglomerative_model.labels_
+clients["class"] = agglomerative_model.labels_
 
-users["class"] = classes
+print(clients[["average_price", "return_rate", "overall_rating", "class"]])
 
-print(users[["average_price", "return_rate", "overall_rating", "class"]])
-
-user_pivot_table = users.pivot_table(index="class",
-                                     values=["average_price", "return_rate", "overall_rating", "customer_id"],
-                                     aggfunc={"average_price": np.mean, "return_rate": np.mean,
-                                              "overall_rating": np.mean, "customer_id": len})
+user_pivot_table = clients.pivot_table(index="class",
+                                       values=["average_price", "return_rate", "overall_rating", "customer_id"],
+                                       aggfunc={"average_price": np.mean, "return_rate": np.mean,
+                                                "overall_rating": np.mean, "customer_id": len})
 print(user_pivot_table)
 
 plt.show()
