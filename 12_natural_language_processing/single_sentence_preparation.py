2c036f3b17096d88e35faae2a7b11c05f60e412d
AndriiLatysh <andrii.latysh@gmail.com>
2020-07-04 14:10:15 +0300

    Minor updates to natural language processing demo.

--- 12_natural_language_processing/single_sentence_preparation.py
+++ 12_natural_language_processing/single_sentence_preparation.py
@@ -11,9 +11,9 @@ imdb_reviews = pd.read_csv("data/IMDB Dataset.csv")
 X = imdb_reviews["review"].iloc[4]
 print(X, "\n")
 
-# lemmatizer = nltk.stem.WordNetLemmatizer()
+lemmatizer = nltk.stem.WordNetLemmatizer()
 # stemmer = nltk.stem.LancasterStemmer()
-stemmer = nltk.stem.PorterStemmer()
+# stemmer = nltk.stem.PorterStemmer()
 
 stop_words = nltk_corpus.stopwords.words("english")
 
@@ -23,8 +23,8 @@ X = X.translate(str.maketrans("", "", string.punctuation))
 
 X = nltk.word_tokenize(X)
 
-# X = [lemmatizer.lemmatize(word) for word in X]
-X = [stemmer.stem(word) for word in X]
+X = [lemmatizer.lemmatize(word) for word in X]
+# X = [stemmer.stem(word) for word in X]
 
 # X = [word for word in X if word not in stop_words]
 
