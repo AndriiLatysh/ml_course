124ace4a682273ed268fc3c0b128a49c2fb52976
AndriiLatysh <andrii.latysh@gmail.com>
2020-12-18 23:02:55 +0200

    Validation for stacking added. Pipelines added. Visualisation removed. Other minor changes.

--- 10_ensemble_learning/double_grade_reevaluated_stacking_classifier.py
+++ 10_ensemble_learning/double_grade_reevaluated_stacking_classifier.py
@@ -1,41 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sklearn.model_selection as sk_ms
+import sklearn.model_selection as sk_model_selection
+import sklearn.metrics as sk_metrics
 import sklearn.preprocessing as sk_preprocessing
 import sklearn.svm as sk_svm
 import sklearn.neural_network as sk_nn
+import sklearn.naive_bayes as sk_maive_bayes
+import sklearn.linear_model as sk_linear
+import sklearn.pipeline as sk_pipeline
 import sklearn.ensemble as sk_ensemble
 
 
-def plot_model(model, title, scaler, qualifies_double_grade_df, subplot):
-    print("Starting plotting {}".format(title))
-
-    subplot.set_title(title)
-
-    qualified_candidates = qualifies_double_grade_df[qualifies_double_grade_df["qualifies"] == 1]
-    unqualified_candidates = qualifies_double_grade_df[qualifies_double_grade_df["qualifies"] == 0]
-
-    max_grade = 101
-    prediction_points = []
-
-    for technical_grade in range(max_grade):
-        for english_grade in range(max_grade):
-            prediction_points.append([technical_grade, english_grade])
-
-    prediction_points = scaler.transform(prediction_points)
-    probability_levels = model.predict_proba(prediction_points)[:, 1]
-    probability_matrix = probability_levels.reshape(max_grade, max_grade)
-
-    subplot.contourf(probability_matrix, cmap="rainbow")  # cmap="RdYlBu"/"binary"
-
-    subplot.scatter(qualified_candidates["technical_grade"], qualified_candidates["english_grade"], color="w")
-    subplot.scatter(unqualified_candidates["technical_grade"], unqualified_candidates["english_grade"], color="k")
-
-    print("Finished plotting {}".format(title))
-
-
-fig, subplots = plt.subplots(nrows=2, ncols=3, figsize=(18, 12))
 print()
 
 qualifies_by_double_grade = pd.read_csv("data/double_grade_reevaluated.csv")
@@ -43,21 +19,24 @@ qualifies_by_double_grade = pd.read_csv("data/double_grade_reevaluated.csv")
 X = qualifies_by_double_grade[["technical_grade", "english_grade"]]
 y = qualifies_by_double_grade["qualifies"]
 
-X_scaler = sk_preprocessing.StandardScaler()
-X = X_scaler.fit_transform(X)
+X_train, X_test, y_train, y_test = sk_model_selection.train_test_split(X, y, test_size=0.25, shuffle=True)
 
-k_folds = sk_ms.StratifiedKFold(n_splits=4, shuffle=True)
+k_folds = sk_model_selection.StratifiedKFold(n_splits=4, shuffle=True)
 
-ann_model = sk_nn.MLPClassifier(hidden_layer_sizes=(10, 10), activation="tanh", max_iter=100000)
-ann_results = sk_ms.cross_val_score(ann_model, X, y, cv=k_folds)
+ann_model = sk_pipeline.Pipeline([("scaler", sk_preprocessing.StandardScaler()),
+                                  ("model",
+                                   sk_nn.MLPClassifier(hidden_layer_sizes=(10, 10), activation="tanh",
+                                                       max_iter=100000))])
+ann_results = sk_model_selection.cross_val_score(ann_model, X_train, y_train, cv=k_folds)
 print("Neural Network accuracy: {:.2f} %".format(ann_results.mean() * 100))
 
-svm_model = sk_svm.SVC(probability=True)
-svm_results = sk_ms.cross_val_score(svm_model, X, y, cv=k_folds)
+svm_model = sk_pipeline.Pipeline([("scaler", sk_preprocessing.StandardScaler()),
+                                  ("model", sk_svm.SVC(probability=True))])
+svm_results = sk_model_selection.cross_val_score(svm_model, X_train, y_train, cv=k_folds)
 print("Support Vector Machine accuracy: {:.2f} %".format(svm_results.mean() * 100))
 
 rfc_model = sk_ensemble.RandomForestClassifier(n_jobs=-1)
-forest_results = sk_ms.cross_val_score(rfc_model, X, y, cv=k_folds)
+forest_results = sk_model_selection.cross_val_score(rfc_model, X_train, y_train, cv=k_folds)
 print("Random Forest accuracy: {:.2f} %".format(forest_results.mean() * 100))
 
 estimators = []
@@ -65,21 +44,12 @@ estimators.append(("ANN", ann_model))
 estimators.append(("SVC", svm_model))
 estimators.append(("RFC", rfc_model))
 
-ensemble_model = sk_ensemble.StackingClassifier(estimators, cv=k_folds)
-ensemble_results = sk_ms.cross_val_score(ensemble_model, X, y, cv=k_folds)
+# meta_estimator = sk_maive_bayes.GaussianNB()
+meta_estimator = sk_linear.LogisticRegression()
+
+ensemble_model = sk_ensemble.StackingClassifier(estimators=estimators, final_estimator=meta_estimator)
+ensemble_model.fit(X_train, y_train)
+ensemble_prediction = ensemble_model.predict(X_test)
 
 print()
-print("Stacking Classifier accuracy: {:.2f} %". format(ensemble_results.mean() * 100))
-print()
-
-ann_model.fit(X, y)
-svm_model.fit(X, y)
-rfc_model.fit(X, y)
-ensemble_model.fit(X, y)
-
-plot_model(ann_model, "Neural Network", X_scaler, qualifies_by_double_grade, subplots[0][0])
-plot_model(svm_model, "Support Vector Machine", X_scaler, qualifies_by_double_grade, subplots[0][1])
-plot_model(rfc_model, "Random Forest", X_scaler, qualifies_by_double_grade, subplots[0][2])
-plot_model(ensemble_model, "Voting Classifier", X_scaler, qualifies_by_double_grade, subplots[1][1])
-
-plt.show()
+print("Stacking Classifier accuracy: {:.2f} %".format(sk_metrics.accuracy_score(y_test, ensemble_prediction) * 100))
