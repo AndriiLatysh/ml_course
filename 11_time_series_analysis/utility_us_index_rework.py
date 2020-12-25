58008563afeeb0a4f31b734f6baf2fd7384fa377
AndriiLatysh <andrii.latysh@gmail.com>
2020-12-26 01:48:34 +0200

    Time series demo reworked and improved.

--- 11_time_series_analysis/utility_us_index_rework.py
+++ 11_time_series_analysis/utility_us_index_rework.py
@@ -7,6 +7,8 @@ import sklearn.metrics as sk_metrics
 import sklearn.model_selection as sk_model_selection
 import statsmodels.tsa.seasonal as statsmodels_seasonal
 import statsmodels.tsa.statespace.sarimax as statmodels_sarimax
+import statsmodels.graphics.tsaplots as statmodels_tsaplots
+import TSErrors
 
 
 def make_cv_splits(data_df, n_splits):
@@ -41,7 +43,7 @@ def average_prediction(train_df, observation_to_predict, **kwargs):
 
 
 def sarima_prediction(train_df, observation_to_predict, **kwargs):
-    sarima_model = statmodels_sarimax.SARIMAX(train_df, order=kwargs["order"], seasonal_order=kwargs["seasonal_order"], freq="MS")
+    sarima_model = statmodels_sarimax.SARIMAX(train_df, order=kwargs["order"], seasonal_order=kwargs["seasonal_order"])
     sarima_model_fit = sarima_model.fit(disp=False)
     values = sarima_model_fit.forecast(observation_to_predict)
     return create_data_frame(values, train_df.index[-1])
@@ -52,72 +54,79 @@ def make_cv_predictions(cv_splits, model, **kwargs):
     for train_df, test_df in cv_splits:
         predicted_df = model(train_df, len(test_df), **kwargs)
         predictions.append(predicted_df)
-    return predictions
+    return pd.concat(predictions)
 
 
-def get_cv_errors(cv_splits, predictions):
-    errors = {"MAE": [], "MSE": [], "RMSLE": []}
-    for z in range(len(predictions)):
-        test_df = cv_splits[z][1]
-        predicted_df = predictions[z]
-        errors["MAE"].append(sk_metrics.mean_absolute_error(test_df["value"], predicted_df["value"]))
-        errors["MSE"].append(sk_metrics.mean_squared_error(test_df["value"], predicted_df["value"]))
-        errors["RMSLE"].append(math.sqrt(sk_metrics.mean_squared_log_error(test_df["value"], predicted_df["value"])))
-    for error_type, error_list in errors.items():
-        errors[error_type] = np.mean(error_list)
-    return errors
+# def get_cv_errors(cv_splits, predictions):
+#     errors = {"MAE": [], "MSE": [], "RMSLE": []}    # TODO test different errors
+#     for z in range(len(predictions)):
+#         test_df = cv_splits[z][1]
+#         predicted_df = predictions[z]
+#         errors["MAE"].append(sk_metrics.mean_absolute_error(test_df["value"], predicted_df["value"]))
+#         errors["MSE"].append(sk_metrics.mean_squared_error(test_df["value"], predicted_df["value"]))
+#         errors["RMSLE"].append(math.sqrt(sk_metrics.mean_squared_log_error(test_df["value"], predicted_df["value"])))
+#     for error_type, error_list in errors.items():
+#         errors[error_type] = np.mean(error_list)
+#     return errors
 
 
-def plot_cv_prediction(predictions):
-    for prediction in predictions:
-        plt.plot(prediction.index, prediction["value"], color="y")
+def calculate_errors(true_df, predicted_df, metrics=("mae", "rmse", "mape")):
+    ts_errors = TSErrors.FindErrors(true_df, predicted_df)
+    all_errors = ts_errors.calculate_all()
+    error_list = {key: all_errors[key] for key in metrics}
+    return error_list
 
 
 register_matplotlib_converters()
-plt.figure(figsize=(20, 10))
 
 utility_index_df = pd.read_csv("data/IPG2211A2N_2020.csv", parse_dates=["DATE"])
 utility_index_df.rename(columns={"DATE": "date", "IPG2211A2N": "value"}, inplace=True)
 utility_index_df.set_index("date", inplace=True)
-
-# utility_index_df = utility_index_df[
-#     (utility_index_df.index >= pd.Timestamp("1979-01-01")) & (utility_index_df.index < pd.Timestamp("2019-01-01"))]
+utility_index_df.index.freq = utility_index_df.index.inferred_freq
 
 utility_index_df = utility_index_df[
-    (utility_index_df.index >= pd.Timestamp("1980-01-01")) & (utility_index_df.index < pd.Timestamp("2020-01-01"))]
+    (utility_index_df.index >= pd.Timestamp("1980-01-01")) & (utility_index_df.index < pd.Timestamp("2020-12-01"))]
 
 print(len(utility_index_df))
 print(utility_index_df.index.min())
 print(utility_index_df.index.max())
 
-plt.plot(utility_index_df.index, utility_index_df["value"])
+# statmodels_tsaplots.plot_acf(utility_index_df)
 
 number_of_splits = 6
 utility_index_cv_splits = make_cv_splits(utility_index_df, number_of_splits)
 
+test_data = pd.concat([t for (_, t) in utility_index_cv_splits])
+
 naive_predictions = make_cv_predictions(utility_index_cv_splits, naive_prediction)
-naive_errors = get_cv_errors(utility_index_cv_splits, naive_predictions)
+naive_errors = calculate_errors(test_data, naive_predictions)
 print("Naive errors:", naive_errors)
-plot_cv_prediction(naive_predictions)
 
 average_predictions = make_cv_predictions(utility_index_cv_splits, average_prediction)
-average_errors = get_cv_errors(utility_index_cv_splits, average_predictions)
+average_errors = calculate_errors(test_data, average_predictions)
 print("Average errors:", average_errors)
-plot_cv_prediction(average_predictions)
 
 sarima_order_kwargs = {"order": (1, 1, 1), "seasonal_order": (1, 1, 1, 12)}
 sarima_predictions = make_cv_predictions(utility_index_cv_splits, sarima_prediction,
                                          **sarima_order_kwargs)
-sarima_errors = get_cv_errors(utility_index_cv_splits, sarima_predictions)
+sarima_errors = calculate_errors(test_data, sarima_predictions)
 print("SARIMA errors:", sarima_errors)
-plot_cv_prediction(sarima_predictions)
+
+plt.figure(figsize=(20, 10))
+
+plt.plot(utility_index_df.index, utility_index_df["value"])
+
+plt.plot(naive_predictions.index, naive_predictions["value"], color="y")
+plt.plot(average_predictions.index, average_predictions["value"], color="y")
+plt.plot(sarima_predictions.index, sarima_predictions["value"], color="y")
 
 sarima_extrapolation = sarima_prediction(utility_index_df, 80, **sarima_order_kwargs)
 plt.plot(sarima_extrapolation.index, sarima_extrapolation["value"], color="g")
 
-utility_index_additive_decomposition = statsmodels_seasonal.seasonal_decompose(utility_index_df, model="additive", freq=12)
+utility_index_additive_decomposition = statsmodels_seasonal.seasonal_decompose(utility_index_df, model="additive")
 utility_index_additive_decomposition.plot()
-utility_index_multiplicative_decomposition = statsmodels_seasonal.seasonal_decompose(utility_index_df, model="multiplicative", freq=12)
+utility_index_multiplicative_decomposition = statsmodels_seasonal.seasonal_decompose(utility_index_df,
+                                                                                     model="multiplicative")
 utility_index_multiplicative_decomposition.plot()
 
 plt.show()
