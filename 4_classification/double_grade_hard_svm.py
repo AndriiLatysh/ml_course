9c20cb5679cfe03eda3e5c369fdc052520d38007
AndriiLatysh <andrii.latysh@gmail.com>
2021-04-03 19:54:40 +0300

    Code readability improved.

--- 4_classification/double_grade_hard_svm.py
+++ 4_classification/double_grade_hard_svm.py
@@ -1,7 +1,6 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sklearn.svm as svm
+import sklearn.svm as sk_svm
 import double_grade_svm_utility
 
 
@@ -12,9 +11,9 @@ double_grade_svm_utility.plot_values(qualifies_double_grade_df)
 X = qualifies_double_grade_df[["technical_grade", "english_grade"]]
 y = qualifies_double_grade_df["qualifies"]
 
-smv_hard_linear_classifier = svm.SVC(kernel="linear")
-smv_hard_linear_classifier.fit(X, y)
+svm_hard_linear_classifier = sk_svm.SVC(kernel="linear")
+svm_hard_linear_classifier.fit(X, y)
 
-double_grade_svm_utility.plot_model(smv_hard_linear_classifier)
+double_grade_svm_utility.plot_model(svm_hard_linear_classifier)
 
 plt.show()
--- 4_classification/double_grade_soft_svm.py
+++ 4_classification/double_grade_soft_svm.py
@@ -1,9 +1,8 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sklearn.model_selection as ms
-import sklearn.metrics as metrics
-import sklearn.svm as svm
+import sklearn.model_selection as sk_model_selection
+import sklearn.metrics as sk_metrics
+import sklearn.svm as sk_svm
 import double_grade_svm_utility
 
 
@@ -14,13 +13,13 @@ double_grade_svm_utility.plot_values(qualifies_double_grade_df)
 X = qualifies_double_grade_df[["technical_grade", "english_grade"]]
 y = qualifies_double_grade_df["qualifies"]
 
-cv_svm_soft_linear_classifier = svm.SVC(kernel="linear")
-cv_svm_soft_linear_predictions = ms.cross_val_predict(cv_svm_soft_linear_classifier, X, y, cv=4)
+cv_svm_soft_linear_classifier = sk_svm.SVC(kernel="linear")
+cv_svm_soft_linear_predictions = sk_model_selection.cross_val_predict(cv_svm_soft_linear_classifier, X, y, cv=4)
 
-cv_confusion_matrix = metrics.confusion_matrix(y, cv_svm_soft_linear_predictions)
+cv_confusion_matrix = sk_metrics.confusion_matrix(y, cv_svm_soft_linear_predictions)
 print(cv_confusion_matrix)
 
-svm_soft_linear_classifier = svm.SVC(kernel="linear")
+svm_soft_linear_classifier = sk_svm.SVC(kernel="linear")
 svm_soft_linear_classifier.fit(X, y)
 
 print(svm_soft_linear_classifier.coef_)
