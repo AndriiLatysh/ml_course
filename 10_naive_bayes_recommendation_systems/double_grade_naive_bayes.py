95c437adf87a27d70b38023a9dbfa7fd13aeb5c4
AndriiLatysh <andrii.latysh@gmail.com>
2021-05-29 01:17:36 +0300

    Naive Bayes classifier demo updated.

--- 10_naive_bayes_recommendation_systems/double_grade_naive_bayes.py
+++ 10_naive_bayes_recommendation_systems/double_grade_naive_bayes.py
@@ -4,29 +4,7 @@ import seaborn as sns
 import sklearn.naive_bayes as sk_naive_bayes
 import sklearn.model_selection as sk_ms
 import sklearn.metrics as sk_metrics
-
-
-def plot_model(model, qualifies_double_grade_df):
-    plt.xlabel("Technical grade")
-    plt.ylabel("English grade")
-
-    qualified_candidates = qualifies_double_grade_df[qualifies_double_grade_df["qualifies"] == 1]
-    unqualified_candidates = qualifies_double_grade_df[qualifies_double_grade_df["qualifies"] == 0]
-
-    max_grade = 101
-    prediction_points = []
-
-    for english_grade in range(max_grade):
-        for technical_grade in range(max_grade):
-            prediction_points.append([technical_grade, english_grade])
-
-    probability_levels = model.predict_proba(prediction_points)[:, 1]
-    probability_matrix = probability_levels.reshape(max_grade, max_grade)
-
-    plt.contourf(probability_matrix, cmap="rainbow")  # cmap="RdYlBu"/"binary"
-
-    plt.scatter(qualified_candidates["technical_grade"], qualified_candidates["english_grade"], color="w")
-    plt.scatter(unqualified_candidates["technical_grade"], unqualified_candidates["english_grade"], color="k")
+import double_grade_utility
 
 
 qualifies_double_grade_df = pd.read_csv("data/double_grade_reevaluated.csv")
@@ -47,6 +25,6 @@ print(confusion_matrix)
 naive_bayes_model.fit(X, y)
 
 plt.figure()
-plot_model(naive_bayes_model, qualifies_double_grade_df)
+double_grade_utility.plot_model(naive_bayes_model, qualifies_double_grade_df)
 
 plt.show()
